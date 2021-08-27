72605780f216cbd2af3422f0d471330cf6af8c98
Max1083 <maximiliano.sanchezv@outlook.com>
2021-08-27 19:59:49 -0400

    Se crea primera parte del ejercicio

---
+++ NinjaMoney/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('process_money',views.process_money,name='process_money'),
+]
---
+++ NinjaMoney/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect
+from random import randint
+# Create your views here.
+def index(request):
+    if 'gold_cant' not in request.session:
+        request.session['gold_cant'] = 0 #Se crea variable de session
+    return render(request,'index.html')
+
+def process_money(request):
+    if "farm" in request.POST: #pregunta si el objeto viene en el request del formulario en su metodo POST
+        ranfarm = randint(10,20) #valor aleatorio entre 10 y 20
+        request.session['gold_cant'] += ranfarm
+    if "cove" in request.POST:
+        rancave = randint(5,10) 
+        request.session['gold_cant'] += rancave
+    if "house" in request.POST:
+        ranhouse = randint(2,5)
+        request.session['gold_cant'] += ranhouse
+    if "casino" in request.POST:
+        request.session['gold_cant'] += randint(-50,50)
+
+    return redirect('/')
